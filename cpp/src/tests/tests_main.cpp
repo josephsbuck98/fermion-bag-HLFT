bfc67a4265166a611d1b24a5682c5ec60c5e2c66
joseph-buck <joeypiano1998@gmail.com>
2025-08-06 15:58:06 -0400

    Unit test skeletons for IO

--- cpp/src/tests/tests_main.cpp
+++ cpp/src/tests/tests_main.cpp
@@ -3,11 +3,13 @@
 void runBondTests();
 void runConfigurationTests();
 void runLatticeTests();
+void runInputTests();
 
 int main(int argc, char** argv) {
     std::cout << "<<<<<RUNNING ALL TESTS>>>>>\n\n";
     runBondTests();
     runConfigurationTests();
     runLatticeTests();
+    runInputTests();
     return 0;
 }
